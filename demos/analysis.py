afe43fe522393e54ad3b5daf33056ef0d3877d10
starmallow <starmallow17@gmail.com>
2025-12-09 23:37:35 -0600

    analysis functions

--- demos/analysis.py
+++ demos/analysis.py
@@ -86,9 +86,10 @@ def SpaceTimePlot(cells, system_density, timesteps, bottleneck=False,
 
 
 
-def DensityFlowResults(cells, system_density, timesteps, random_state=None, 
-                       sample_spacing=100, first_sample=50,
-                       v_max=5, p=0.5, t0=None, initial_state=None):
+def DensityFlowResults(cells, system_density, timesteps, bottleneck=False, 
+                       random_state=None, sample_spacing=100, first_sample=50,
+                       v_max=5, p=0.5, t0=None, initial_state=None,
+                       bn_start=None, bn_end=None, v_max_bn=1, inflow=0.5):
     """
     Function to streamline the collection of traffic flow vs. density data from the 
     Nagel–Schreckenberg models. 
@@ -117,9 +118,23 @@ def DensityFlowResults(cells, system_density, timesteps, random_state=None,
             is used.
         initial_state (None or array): The initial state of the system. If None, a 
             random initial state is used.
+
+        bn_start (int or None): The starting cell index of the bottleneck region. If None, no bottleneck
+            region is used.
+        bn_end (int or None): The ending cell index of the bottleneck. If None, there is no bottleneck.
+        v_max_bn (int): The maximum veolcity inside the bottleneck region.
+        inflow (float): The probability that a new car enters the traffic lane at index 0 when that cell
+            is empty.
     """
 
     cars = int(system_density * cells)
+    if bottleneck:
+        model = TrafficBottleneck(cars, cells=cells, v_max=v_max, p=p, t0=t0, random_state=random_state, 
+                                  initial_state=initial_state, bn_start=bn_start, bn_end=bn_end, 
+                                  v_max_bn=v_max_bn, inflow=inflow)
+    else:
+        model = TrafficModelCircular(cars, cells, v_max, p, t0, random_state, initial_state)
+
     model = TrafficModelCircular(cars, cells, v_max, p, t0, random_state, initial_state)
     model.simulate(timesteps)
     history = np.asarray(model.history)
@@ -133,9 +148,6 @@ def DensityFlowResults(cells, system_density, timesteps, random_state=None,
 
     flow_conditions = [transpose[sample_indices-i]>i for i in range(v_max)]
     flow = np.where(np.logical_or.reduce(flow_conditions), 1, 0)
-    #
-    # print(f"flow conditions: {flow_conditions}\nflow locations: {flow}\n")
-    #
     flow = np.sum(flow, axis=1, dtype=float)
     flow /= timesteps
 
@@ -145,3 +157,71 @@ def DensityFlowResults(cells, system_density, timesteps, random_state=None,
 
 
 
+def PlotDensityFlow(cells, timesteps, bottleneck=False, save_file_name="",
+                    densities=[0.02, 0.04, 0.06, 0.08, 0.1, 0.15, 0.2],
+                    random_state=None, sample_spacing=100, first_sample=50,
+                    v_max=5, p=0.5, t0=None, initial_state=None,
+                    bn_start=None, bn_end=None, v_max_bn=1, inflow=0.5):
+    """
+    Function to streamline plotting of several runs of traffic flow vs. density data from the 
+    Nagel–Schreckenberg models, with varying system densities.
+    Displays the plot in jupyter notebook. Optionally saves the .jpg file.
+
+    Parameters:
+        cells (int): The number of cells in the traffic lane, which can be occupied 
+            by 0 or 1 car.
+        densities (list of floats): List of the total density of cars in the system for each
+            data run to be plotted. The number of cars passed to the traffic model will equal 
+            int(system_density * cells). Floats must be between 0 and 1.
+        timesteps (int): The number of timesteps to simulate for data collection.
+        random_state (None or int): The seed for the random number generator. If None,
+            the random number generator is not seeded.
+
+        sample_spacing (int): Steps to take between the sampled cell indices used for 
+            data collection. Defaults to 100.
+        first_sample (int): First index to start the sample cell selection with. 
+            Defaults to 50.
+
+        v_max (int): Speed limit of the traffic system.
+        p (float): Probability factor for random decceleration events. Float must 
+            be between 0 and 1.
+        t0 (int): Initial number of timesteps to run the system through to reach a 
+            state of equilibrium before data collection starts. If None, 10 * cells 
+            is used.
+        initial_state (None or array): The initial state of the system. If None, a 
+            random initial state is used.
+
+        bn_start (int or None): The starting cell index of the bottleneck region. If None, no bottleneck
+            region is used.
+        bn_end (int or None): The ending cell index of the bottleneck. If None, there is no bottleneck.
+        v_max_bn (int): The maximum veolcity inside the bottleneck region.
+        inflow (float): The probability that a new car enters the traffic lane at index 0 when that cell
+            is empty.
+    """
+
+    # cars = (np.asarray(densities) * cells).astype(int)
+    density_data = np.empty((2,))
+    flow_data = np.empty((2,))
+
+    for system_density in densities:
+        dens_temp, flow_temp = DensityFlowResults(cells, system_density, timesteps, bottleneck=False, 
+                                                random_state=None, sample_spacing=100, first_sample=50,
+                                                v_max=5, p=0.5, t0=None, initial_state=None,
+                                                bn_start=None, bn_end=None, v_max_bn=1, inflow=0.5)
+        density_data = np.hstack((density_data.copy(), dens_temp.copy()))
+        flow_data = np.hstack((flow_data.copy(), flow_temp.copy()))
+    
+    plt.figure(dpi=300)
+    plt.plot(density_data, flow_data, ".k", ms=3, alpha=0.5)
+
+    plt.ylim(0,0.45)
+    plt.ylabel("Flow (car/timestep)")
+    plt.xlabel("Density (cell occupancy / timestep)")
+
+    if save_file_name:
+        file = save_file_name + ".jpg"
+        # put the file in specific folder ?
+        plt.savefig(file, format='jpg', bbox_inches='tight')
+    
+    plt.show()
+    
