18a68af0c627c3639b6f2efbc6f39f64f3631734
natesafranek <nathansafranek@utexas.edu>
2025-12-09 22:56:15 -0600

    Refactor plots.py by removing simulation code
    Removed the traffic simulation code and plotting logic.

--- extras/plots.py
+++ extras/plots.py
@@ -7,27 +7,3 @@ from nsmodel import *
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-timesteps = 100
-cars = 15
-cells = 100
-
-test2 = TrafficModelCircular(cars, cells)
-test2.simulate(timesteps)
-data = test2.history
-
-fig, ax = plt.subplots(figsize=(12, timesteps//10), dpi=300)
-im = ax.imshow(data, cmap='Greys')
-
-# ax.set_xticks([])
-# ax.set_yticks([])
-ax.tick_params(axis='x', labelsize=8)
-ax.tick_params(axis='y', labelsize=8)
-
-ax.set_ylabel("Timestep", fontsize=12)
-ax.set_xlabel("Cell Index", fontsize=12)
-ax.set_title("Time Evolution of Single, Circular Traffic Lane", fontsize=14)
-cbar = plt.colorbar(im, ax=ax, pad=0.02)
-cbar.set_label(f"Car Velocity\n(-1 indicates no cars present)", fontsize=10, labelpad=1)
-
-plt.show()
